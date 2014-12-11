5cf452481a3295f81273ac8ae22d0e18f62a8c5c
Vicki Long <tovickilong@gmail.com>
2014-12-11 08:41:15 -0500

    Changed layout of index activity and added button listener to next activity

--- src/long0/vicki/gestures/GesturesIndexActivity.java
+++ src/long0/vicki/gestures/GesturesIndexActivity.java
@@ -1,19 +1,38 @@
 package long0.vicki.gestures;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class GesturesIndexActivity extends ActionBarActivity {
+public class GesturesIndexActivity extends Activity {
+	private Button mStartButton;
+	private Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gestures_index);
+        
+        getInit();
     }
-
+    
+    public void getInit () {
+    	mStartButton = (Button) findViewById(R.id.start_gestures_button);
+    }
+    
+    public void addClickListener () {
+    	mStartButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	i = new Intent(GesturesIndexActivity.this, GesturesActivity.class);
+            	startActivityForResult(i, 0);
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
