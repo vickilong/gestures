71e1791c911c23c19d4beea3841cd10b17bc1f0c
Vicki Long <tovickilong@gmail.com>
2014-12-17 08:22:55 -0500

    Added ResultsActivity

---
+++ src/long0/vicki/gestures/ResultsActivity.java
@@ -0,0 +1,13 @@
+package long0.vicki.gestures;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ResultsActivity extends Activity {
+	
+	protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+    }
+	
+}
