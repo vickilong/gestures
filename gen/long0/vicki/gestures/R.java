121019c05165742f34e4d64dfc98f5152273ed06
Vicki Long <tovickilong@gmail.com>
2014-12-17 08:44:08 -0500

    Added timer

--- gen/long0/vicki/gestures/R.java
+++ gen/long0/vicki/gestures/R.java
@@ -1091,7 +1091,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050041;
+        public static final int action_settings=0x7f050042;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1104,7 +1104,7 @@ containing a value of this type.
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
-        public static final int exit_gestures_button=0x7f050040;
+        public static final int exit_gestures_button=0x7f050041;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
         public static final int home=0x7f050014;
@@ -1121,8 +1121,8 @@ containing a value of this type.
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
-        public static final int restart_button=0x7f05003f;
-        public static final int results=0x7f05003e;
+        public static final int restart_button=0x7f050040;
+        public static final int results=0x7f05003f;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1138,9 +1138,10 @@ containing a value of this type.
         public static final int showHome=0x7f050004;
         public static final int showTitle=0x7f050006;
         public static final int split_action_bar=0x7f05001e;
-        public static final int start_gestures_button=0x7f05003d;
+        public static final int start_gestures_button=0x7f05003e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
+        public static final int timer=0x7f05003d;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1251,11 +1252,12 @@ containing a value of this type.
         public static final int app_name=0x7f0a000d;
         public static final int exit_text=0x7f0a0012;
         public static final int hello_world=0x7f0a000e;
-        public static final int restart_text=0x7f0a0014;
-        public static final int results_text=0x7f0a0013;
+        public static final int restart_text=0x7f0a0015;
+        public static final int results_text=0x7f0a0014;
         public static final int start_text=0x7f0a0011;
+        public static final int timer_text=0x7f0a0013;
         public static final int title=0x7f0a0010;
-        public static final int title_activity_gestures_screen=0x7f0a0015;
+        public static final int title_activity_gestures_screen=0x7f0a0016;
     }
     public static final class style {
         /** 
--- src/long0/vicki/gestures/GesturesActivity.java
+++ src/long0/vicki/gestures/GesturesActivity.java
@@ -1,19 +1,33 @@
 package long0.vicki.gestures;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v4.app.FragmentActivity;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class GesturesActivity extends FragmentActivity implements ButtonFragment.ButtonListener {
+	private TextView mTimer;
 	
 	protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gestures);
         
-        getInit();
+        mTimer = (TextView) findViewById(R.id.timer);
+        
+        startTimer();
     }
     
-    public void getInit () {
-    	
+    public void startTimer () {
+    	new CountDownTimer(60000, 1000) {
+    	     public void onTick(long millisUntilFinished) {
+    	         mTimer.setText("seconds remaining: " + millisUntilFinished / 1000);
+    	     }
+
+    	     public void onFinish() {
+    	         mTimer.setText("done!");
+    	     }
+    	}.start();
     }
     
     public void onButtonClick() {
